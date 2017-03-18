db88827a9aa1fb5e6181ed224d9e2973ef48cf16
Marco Asbreuk <marco@itsasbreuk.nl>
2017-03-18 12:05:10 +0100

    Changed React modules mixin into js modules

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,5 +1,5 @@
 
-exports.CartesianChartPropsMixin = require('./CartesianChartPropsMixin.jsx');
-exports.DefaultAccessorsMixin = require('./DefaultAccessorsMixin.jsx');
-exports.ViewBoxMixin = require('./ViewBoxMixin.jsx');
-exports.TooltipMixin = require('./TooltipMixin.jsx');
+exports.CartesianChartPropsMixin = require('./cartesianChartPropsMixin');
+exports.DefaultAccessorsMixin = require('./defaultAccessorsMixin');
+exports.ViewBoxMixin = require('./viewBoxMixin');
+exports.TooltipMixin = require('./tooltipMixin');
